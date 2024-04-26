f2c8c7b01e22e225c21a3f813b6b712d044d9e2a
Lucas Teixeira de Souza <161272902+Luks715@users.noreply.github.com>
2024-04-26 18:13:33 -0300

    remover as resources para movie_goer e movie_review

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources 'movies'
-  resources 'movie_goer'
-  resources 'movie_review'
-
   root 'movies#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
