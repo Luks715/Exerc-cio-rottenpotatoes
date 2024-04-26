99359d9a81d385fe0d53faead372795bf2f71db0
Lucas Teixeira de Souza <161272902+Luks715@users.noreply.github.com>
2024-04-26 17:24:51 -0300

    Update da versão do Ruby para 3.3.0 e correção da migração da DB

--- db/migrate/20240426192749_dbmigration.rb
+++ db/migrate/20240426192749_dbmigration.rb
@@ -3,6 +3,7 @@ class Dbmigration < ActiveRecord::Migration[7.1]
     create_table :movies do |t|
       t.string 'title'
       t.string 'rating'
+      t.string 'length'
       t.text 'description'
       t.datetime 'release_date'
       # Add fields that let Rails automatically keep track
