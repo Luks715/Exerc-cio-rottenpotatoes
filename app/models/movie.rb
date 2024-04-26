3c0b70f953727e3b438df37be5511295eb53d550
Lucas Teixeira de Souza <161272902+Luks715@users.noreply.github.com>
2024-04-26 14:48:01 -0300

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,2 @@
 class Movie < ActiveRecord::Base
-  validates :title, presence: true
-  validates :rating, presence: true
-  validates :length, presence: true
-  validates :description, presence: true
-  validates :release_date, presence: true
 end
